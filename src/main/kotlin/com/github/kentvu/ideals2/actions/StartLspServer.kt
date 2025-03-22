f36c7470a7de1876e2ba15d786eed96e88b03b7b
Kiên T Vu <vutrankien.vn@gmail.com>
2025-03-22 11:57:05 +0000

    Example action

---
+++ src/main/kotlin/com/github/kentvu/ideals2/actions/StartLspServer.kt
@@ -0,0 +1,34 @@
+package com.github.kentvu.ideals2.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.ui.Messages
+
+
+class StartLspServer : AnAction() {
+    override fun update(e: AnActionEvent) {
+        // Set the availability based on whether a project is open
+        val project = e.project
+        e.presentation.isEnabledAndVisible = project != null
+    }
+
+    override fun actionPerformed(event: AnActionEvent) {
+        // Using the event, create and show a dialog
+        val currentProject = event.project
+        val message =
+            StringBuilder(event.presentation.text + " Selected!")
+        // If an element is selected in the editor, add info about it.
+        val selectedElement = event.getData(CommonDataKeys.NAVIGATABLE)
+        if (selectedElement != null) {
+            message.append("\nSelected Element: ").append(selectedElement)
+        }
+        val title = event.presentation.description
+        Messages.showMessageDialog(
+            currentProject,
+            message.toString(),
+            title,
+            Messages.getInformationIcon()
+        )
+    }
+}
